963c5a31a44cc3dc89e1dd97a66e9c1cb55b2492
Hiroki Takahashi <git@hiroki-t.com>
2021-12-19 13:39:46 +0200

    Updated styling

--- src/components/Album/ThumbnailGrid/useStyles.ts
+++ src/components/Album/ThumbnailGrid/useStyles.ts
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
   gifCard: {
+    cursor: 'pointer',
     height: '100%',
     display: 'flex',
     flexDirection: 'column',
