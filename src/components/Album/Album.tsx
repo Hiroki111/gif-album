8d9c4945880b075dae65af76fd3f2b42f9bdd47b
Hiroki Takahashi <git@hiroki-t.com>
2021-12-20 22:22:33 +0200

    Made sure that refreshing the page or using the search feature will scroll to the top of the browser

--- src/components/Album/Album.tsx
+++ src/components/Album/Album.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { NavBar } from './NavBar';
 import { Footer } from './Footer';
 import { GifModal } from './GifModal';
@@ -6,6 +8,17 @@ import { useStyles } from './useStyles';
 
 export function Album() {
   const classes = useStyles();
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', scrollToTop);
+    return () => {
+      window.removeEventListener('beforeunload', scrollToTop);
+    };
+  }, []);
+
+  function scrollToTop() {
+    window.scrollTo(0, 0);
+  }
 
   return (
     <div className={classes.root}>
